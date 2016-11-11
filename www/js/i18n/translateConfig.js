9b0d12c80b338ef7852b33d1fe553285ea00c376
Usuario <Usuario@Usuario-PC>
2016-11-10 22:22:12 -0400

    i18n

---
+++ www/js/i18n/translateConfig.js
@@ -0,0 +1,9 @@
+angular.module('app.controllers').config(function ($translateProvider, $translatePartialLoaderProvider) {
+    $translateProvider.useSanitizeValueStrategy('escape');
+    $translateProvider.useLoader('$translatePartialLoader', {
+        urlTemplate: 'js/i18n/{part}/locale-{lang}.json'
+    });
+    $translatePartialLoaderProvider.addPart('home');
+    $translateProvider.preferredLanguage('es');
+    $translateProvider.forceAsyncReload(true);
+});
